41e15e5fac4a2bbf1312084bf86ea00328e60e1a
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-29 17:04:37 -0400

    Mira Mesa Market 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -145,6 +145,7 @@
                     try {
                         let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/miramesa-contact-us.json" }), this.$store.dispatch("getData", "repos")]);
                         return results;
+                        console.log(results)
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
