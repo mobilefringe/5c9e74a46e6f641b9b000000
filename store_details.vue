9920044c61e7037b066c78870187afa1fe22c6ad
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-05 14:17:41 -0400

    Mira Mesa Market 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -26,7 +26,7 @@
                                 </div>
                                 <div id="store_dets_logo_container" v-else>
                                     <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="logo">
-                    			    <img  class="store_details_image" :src="currentStore.store_front_url_abs" alt="">
+                    			    <img  class="store_details_image" :src="currentStore.store_front_url_abs" :alt="currentStore.name">
                                 </div>
                                 <div class="margin_20 center" v-if="currentStore.phone">
                                     <a class="store_details_phone" :href="'tel:' + currentStore.phone">{{ currentStore.phone }}</a>    
