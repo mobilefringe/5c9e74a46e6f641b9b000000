e55cd500d1e13e91df4fb6d52a9c7b941bc908cc
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 12:34:12 -0400

    Mira Mesa Market 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -126,6 +126,7 @@
                     
                     if(response && response[0]){
                         this.main = response[0].data
+                        console.log(this.main)
                         if(response[0].data && response[0].data.subpages){
                            this.contactInfo = response[0].data.subpages[1];
                         }
