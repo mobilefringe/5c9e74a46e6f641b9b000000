e7c33c55e376dbf488f3bf04d1a4cad6bbe496e7
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-10 14:31:26 -0400

    Mira Mesa Market 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -21,7 +21,9 @@
                                 </div>
                             </div>
                             <div v-else-if="!banner.url">
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
+                                    <p style="display: none">{{ banner.name }}</p>
+                                </div>
                             </div>
                             <div v-else>
                                 <a :href="banner.url">
