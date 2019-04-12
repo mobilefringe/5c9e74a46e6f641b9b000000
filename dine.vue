0f8fdcd9a368f9a465164ea18c5d42b0340d92f8
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-12 10:52:59 -0400

    Mira Mesa Market 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -56,12 +56,12 @@
                             	        <router-link :to="'/stores/'+ store.slug">
                                 			<!--<img class="store_img" :src="store.image_url" alt="">-->
                                 			<div v-if="!store.no_store_logo">
-                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5c9e74a46e6f641b9b000000/image/png/1554995111000/miramesa_default.png">
+                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png">
                                 			    <img  class="store_img" :src="store.store_front_url_abs" alt="">
                                 			</div>
                                 			
                                             <div v-else class="no_logo_container">
-                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5c9e74a46e6f641b9b000000/image/png/1554995111000/miramesa_default.png" alt="">
+                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                                 <div class="no_logo_text">
                                                     <div class="store_text"><h4>{{ store.name }}</h4></div>
                                                 </div>
