fb20d9bbf774ee8e7a581a309bbff56101667d18
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-12 10:42:44 -0400

    Mira Mesa Market 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -244,7 +244,7 @@
                         var showOnWebDate = moment.tz(value.show_on_web_date, this.timezone).format();
                         if (today >= showOnWebDate) {
                             if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1529532187000/eventsplaceholder2@2x.png";
+                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5c9e74a46e6f641b9b000000/image/png/1554995111000/miramesa_default.png";
                             }
                             // Sort Featured Events
                             if (value.is_featured) {
