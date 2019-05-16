b69b4c33dc21e7301db84894e0de8b896ef0fe1e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-16 19:24:56 -0400

    Mira Mesa Market 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -174,14 +174,12 @@
                     var store_list = [];
                     var vm = this;
                     _.forEach(this.processedStores, function(value, key) {
-                        if(!_.includes(value.categories, vm.dineFilter)) {
-                            if (_.includes(value.image_url, 'missing')) {
-                               value.no_store_logo = true;
-                            } else {
-                              value.no_store_logo = false;
-                            }
-                            store_list.push(value);
+                        if (_.includes(value.image_url, 'missing')) {
+                           value.no_store_logo = true;
+                        } else {
+                          value.no_store_logo = false;
                         }
+                        store_list.push(value);
                     });
                     this.filteredStores = store_list;
                     return store_list
