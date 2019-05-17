1553e8bebee3dc64a7f3e74d3578ec8c789da9c7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-16 20:22:56 -0400

    Mira Mesa Market 2.0 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -31,45 +31,6 @@ define([], function () {
                     ]
                 },
                 {
-                    path: '/coupons',
-                    component: view('default'),
-                    meta: {
-                        breadcrumb: 'Coupons',
-                    },
-                    children: [
-                        {
-                            path: '',
-                            component: view('coupons'),
-                            
-                            name: 'coupons'
-                        },
-                        {
-                            path: '/coupons/my-basket',
-                            component: view('default'),
-                            children: [
-                                {
-                                    path: '',
-                                    component: view('coupon_basket'),
-                                    meta: {
-                                        breadcrumb: 'My Basket',
-                                    },
-                                    name: 'couponBasket',
-                                    props: true
-                                }
-                            ]
-                        },
-                        {
-                            path: ':id',
-                            component: view('coupons_details'),
-                            meta: {
-                                breadcrumb: 'Coupon Details',
-                            },
-                            name: 'couponDetails',
-                            props: true
-                        }
-                    ]
-                },
-                {
                     path: '/leasing',
                     component: view('default'),
                     children: [
@@ -114,52 +75,6 @@ define([], function () {
             ]
         },
         {
-            path: '/events',
-            component: view('default'),
-            meta: {
-                breadcrumb: 'Events',
-            },
-            children: [
-                {
-                    path: '',
-                    component: view('events_and_promotions'),
-                    name: 'Events and Promotions'
-                },
-                {
-                    path: ':id',
-                    component: view('event_details'),
-                    meta: {
-                        breadcrumb: 'Event Details',
-                    },
-                    name: 'eventDetails',
-                    props: true
-                }
-            ]
-        },
-        {
-            path: '/promotions',
-            component: view('default'),
-            meta: {
-                breadcrumb: 'Sales & Promotions',
-            },
-            children: [
-                {
-                    path: '',
-                    component: view('events_and_promotions'),
-                    name: 'Events and Promotions'
-                },
-                {
-                    path: ':id',
-                    component: view('promotion_details'),
-                    meta: {
-                        breadcrumb: 'Promotion Details',
-                    },
-                    name: 'promotionDetails',
-                    props: true
-                }
-            ]
-        },
-        {
             path: '/events-and-promotions',
             component: view('default'),
             meta: {
@@ -170,6 +85,24 @@ define([], function () {
                     path: '',
                     component: view('events_and_promotions'),
                     name: 'events_and_promotions'
+                },
+                {
+                    path: '/events/:id',
+                    component: view('event_details'),
+                    meta: {
+                        breadcrumb: 'Event Details',
+                    },
+                    name: 'eventDetails',
+                    props: true
+                },
+                {
+                    path: '/promotions/:id',
+                    component: view('promotion_details'),
+                    meta: {
+                        breadcrumb: 'Promotion Details',
+                    },
+                    name: 'promotionDetails',
+                    props: true
                 }
             ]
         },
@@ -251,29 +184,6 @@ define([], function () {
                         }
                     ]
                 },
-            ]
-        },
-        {
-            path: '/posts',
-            component: view('default'),
-            meta: {
-                breadcrumb: 'Blog',
-            },
-            children: [
-                {
-                    path: '',
-                    component: view('posts'),
-                    name: 'posts'
-                },
-                {
-                    path: ':id',
-                    component: view('posts_details'),
-                    meta: {
-                        breadcrumb: 'Blog Details',
-                    },
-                    name: 'postsDetails',
-                    props: true
-                }
             ]
         },
         {
