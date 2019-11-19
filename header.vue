ef10e84dfd678b4cff8dab49df52f2539af8ddec
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-19 15:36:27 -0500

    Mira Mesa Market 2.0 1.2  /header.vue

--- header.vue
+++ header.vue
@@ -50,7 +50,7 @@
         							        <router-link :to="item.href" v-if="item.sub_menu == undefined">
         							            <div class="btn-block">{{$t(item.name)}}</div>
     							            </router-link>
-        							        <div v-else role="tablist">
+        							        <div v-else >
                                                 <b-card no-body class="mb-1">
                                                     <b-card-header header-tag="header" class="p-1" role="tab">
                                                         <b-btn block href="#" v-b-toggle="$t(item.name)" variant="info">
