0c54d32052616351de921c29ad60aa77bdb7649a
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-19 16:43:45 -0500

    Mira Mesa Market 2.0 1.2  /header.vue

--- header.vue
+++ header.vue
@@ -6,7 +6,7 @@
                     <div class="col-md-12">
                         <div class="social_icon_container">
     					    <span class="social_icon" v-for="item in social_media">
-                                <a :href="item.url" target="_blank">
+                                <a :href="item.url" target="_blank" :aria-label="'Follow Us on ' + item.name">
                                     <p class="accessibility">{{item.name}}</p>
                                     <i :class="item.iconClass" aria-hidden="true"></i>
                                 </a>
