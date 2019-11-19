40a06f7507b1456d1a35ecadbae4d4c4226df016
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-19 15:13:13 -0500

    Mira Mesa Market 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div><!-- without an outer container div this component template will not render -->
+    <main><!-- without an outer container div this component template will not render -->
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
@@ -103,7 +103,7 @@
                 </div>
             </div>
         </transition>
-    </div>
+    </main>
 </template>
 <script>
     define(["Vue", "vuex", "vue!vue-slick", "moment", "moment-timezone", "vue-moment", "vue!welcome_msg"], function (Vue, Vuex, slick, moment, tz, VueMoment, welcomeMessage) {
