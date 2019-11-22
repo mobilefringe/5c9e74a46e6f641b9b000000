0b2931248fc984a6b80fc49eb425742fda90a219
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-22 12:28:04 -0500

    Mira Mesa Market 2.0 1.2  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -260,7 +260,7 @@
                             temp_promo.push(current_promo);
                         }); 
                         this.storePromotions = temp_promo;
-                        console.log(his.storePromotions)
+                        console.log(this.storePromotions)
     
                         var vm = this;
                         var temp_event = [];
